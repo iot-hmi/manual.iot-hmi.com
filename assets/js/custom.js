1cbf6ab1198c6af4bec7a517fe59a7bd17c4d6be
Johnny Tsai <johnny.tsai@iot-hmi.com>
2021-09-08 17:48:49 +0800

    sit-nav auto scroll

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,4 +5,5 @@ $( document ).ready(function() {
     $(".ver_select").on( "change", function() {
         window.location.href = $(this).val();
     });
+    $("#site-nav").scrollTop($("a.nav-list-link.active").offset().top-60-($("#site-nav").height()/2));
 });
